a0c7f380010d9a5f274d11474f92b045e3a98241
askvictor <askvictor@gmail.com>
2019-06-16 11:18:20 +1000

    now caches files for one week

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import requests
 from feedgen.feed import FeedGenerator
 import datetime
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, send_file
+from pathlib import Path
+import re
 app = Flask(__name__)
+
+CACHE_DIR = "cache"
 
 all_shows_url = 'https://airnet.org.au/rest/stations/3pbs/programs'
 
@@ -24,6 +28,19 @@ def all_shows():
 @app.route("/<slug>.xml")
 def pbs_show(slug):
     print('processing %s' % slug)
+
+    p = Path(CACHE_DIR)
+    cache_glob = list(p.glob(slug + "*"))
+    if cache_glob:
+        recent_cache_path = sorted(cache_glob)[-1]
+        cache_time_str = re.search(slug + '.([^\.]+).xml', recent_cache_path.name).group(1)
+        cache_time = datetime.datetime.strptime(cache_time_str, '%Y-%m-%dT%H:%M:%S')
+        print(cache_time_str)
+        print(datetime.datetime.now().isoformat())
+        if cache_time + datetime.timedelta(days=7) > datetime.datetime.now():
+            # cached file is still valid; return that
+            return send_file(recent_cache_path.open(), mimetype='application/rss+xml')
+
     show_url = show_format.format(slug=slug)
     show_info = requests.get(show_url).json()
     show_title = show_info['name']
@@ -46,9 +63,10 @@ def pbs_show(slug):
     feed.language('en')
 
     episodes = requests.get(show_info['episodesRestUrl']).json()
-
+    episode_times = []
     for episode in reversed(episodes):
         start_time = datetime.datetime.strptime(episode['start'], '%Y-%m-%d %H:%M:%S')
+        episode_times.append(start_time)
         title = "{} {}".format(show_title, start_time.date())
         media_url = media_format.format(slug=slug, timestamp=start_time.strftime("%Y%m%d%H%M"))
 
@@ -64,6 +82,12 @@ def pbs_show(slug):
             feed_entry.description(tracklist)
         except:
             feed_entry.description(title)
+    if episode_times:
+        # remove all old cache files for this program
+        for cachefile in p.glob(slug+"*"):
+            cachefile.unlink()
 
+        recent_ep_time = sorted(episode_times)[-1].isoformat()
+        feed.rss_file(CACHE_DIR + "/" + slug + " " + recent_ep_time + ".xml", pretty=True)
     return Response(feed.rss_str(pretty=True), mimetype='application/rss+xml')
 
